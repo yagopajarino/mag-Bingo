1dbe2439dd683d7e565df547cc981b830c805c26
Yago Pajariño <yago977@hotmail.com>
2021-08-18 10:00:20 -0300

    excel canciones

---
+++ musical/get_songs.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json
+
+j = open("tracks.json","r")
+data = json.load(j)
+
+songs = []
+artists = []
+for x in data["items"]:
+    song = x["track"]["name"]
+    artist = x["track"]["album"]["artists"][0]["name"]
+    songs.append(song)
+    artists.append(artist)
+
+df = pd.DataFrame(artists,columns=["Artista"])
+df["Canción"] = songs
+
+df.to_excel("songs.xlsx", index=False)
